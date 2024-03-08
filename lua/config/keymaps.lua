a028c406a1a73fe5ec4e140b84875e8e1d7731c9
tenthree <ten.three@hotmail.com>
2024-03-08 15:37:43 +0800

    add border to default terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add border to terminal
+-- NOTE: https://github.com/LazyVim/LazyVim/discussions/2092#discussioncomment-8662180
+local Util = require("lazyvim.util")
+vim.keymap.set("n", "<C-/>", function()
+  Util.terminal(nil, { border = "rounded" })
+end, { desc = "Terminal with border" })
