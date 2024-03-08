4cdf16b7a355d6118044e82d8afecec0a1854810
tenthree <ten.three@hotmail.com>
2024-03-08 15:38:43 +0800

    add border to lsp preview info

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,9 @@
+-- https://github.com/LazyVim/LazyVim/issues/1942#issuecomment-1793440318
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+    },
+  },
+}
